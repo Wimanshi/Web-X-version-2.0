fe9f03111e72aa68fcb9d1f0d3e9899798820c56
Wimanshi <shashiniabeywickrama97@gmail.com>
2019-07-12 13:47:28 +0530

    add header and footer to the profile

--- indexProfile.php
+++ indexProfile.php
@@ -73,9 +73,155 @@ require_once ('class.Database.php');
 
 	</head>
 	<body>
-		
-	<div class="fh5co-loader"></div>
-	
+    <!-- Preloader -->
+    <div id="preloader">
+        <div class="wrapper">
+            <div class="cssload-loader"></div>
+        </div>
+    </div>
+
+    <!-- ***** Top Search Area Start ***** -->
+    <div class="top-search-area">
+        <!-- Search Modal -->
+        <div class="modal fade" id="searchModal" tabindex="-1" role="dialog" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <!-- Close Button -->
+                        <button type="button" class="btn close-btn" data-dismiss="modal"><i class="fa fa-times"></i></button>
+                        <!-- Form -->
+                        <form action="index.php" method="post">
+                            <input type="search" name="top-search-bar" class="form-control" placeholder="Search and hit enter...">
+                            <button type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- ***** Top Search Area End ***** -->
+
+    <!-- ***** Header Area Start ***** -->
+    <header class="header-area">
+    <link rel="stylesheet" href="style.css">
+            <!-- Main Header Start -->
+            <div class="main-header-area">
+                <div class="classy-nav-container breakpoint-off">
+                    <!-- Classy Menu -->
+                    <nav class="classy-navbar justify-content-between" id="uzaNav">
+    
+                        <!-- Logo -->
+                        <a class="nav-brand" href="index.php"><img src="./img/core-img/logo.png" alt=""></a>
+    
+                        <!-- Navbar Toggler -->
+                        <div class="classy-navbar-toggler">
+                            <span class="navbarToggler"><span></span><span></span><span></span></span>
+                        </div>
+    
+                        <!-- Menu -->
+                        <div class="classy-menu">
+                            <!-- Menu Close Button -->
+                            <div class="classycloseIcon">
+                                <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                            </div>
+    
+                            <!-- Nav Start -->
+                        <div class="classynav">
+                                <ul id="nav">
+                                    <li class="current-item"><a href="./index.php">Home</a></li>
+                                    <li><a href="#">Pages</a>
+                                        <ul class="dropdown">
+                                            <li><a href="./index.php">- Home</a></li>
+                                            <li><a href="./start exploring.php">- Start Exploring</a></li>
+                                            <li><a href="./about.php">- About</a></li>
+                                            <li><a href="./services.php">- Services</a></li>
+                                            <li><a href="#">- Developer List</a>
+                                                <ul class="dropdown">
+                                                    <li><a href="./allDevelopers.php">- All Developers</a>
+                                                        <ul class="dropdown">
+                                                            <li><a href="./android.php">- Android Developing</a></li>
+                                                            <li><a href="./graphic.php">- Graphic Designing</a></li>
+                                                            <li><a href="./ios.php">- iOS Developing</a></li>
+                                                            <li><a href="./website.php">- Website Developing</a></li>
+                                                            <li><a href="./video.php">- Video Editing</a></li>
+                                                        </ul>
+                                                    </li>
+                                                    <!--li><a href="#">- Dropdown Item</a></li>
+                                                    <li><a href="#">- Dropdown Item</a></li-->
+                                                </ul>
+                                            </li>
+                                            <li><a href="./portfolio.php">- Portfolio</a></li>
+                                            <li><a href="./portfolio-single.php">- Single Portfolio</a></li>
+                                            <li><a href="./blog.php">- Blog</a></li>
+                                            <li><a href="./single-blog.php">- Blog Details</a></li>
+                                            <li><a href="./contact.php">- Contact</a></li>
+                                        </ul>
+                                    </li>
+                                    <!--li><a href="./portfolio.php">Portfolio</a></li-->
+                                    <li><a href="./about.php">About</a></li>
+                                    <li><a href="#">DeveloperList</a>
+                                        <ul class="dropdown">
+                                            <li><a href="./allDevelopers.php">- All Developers</a></li>
+                                            <li><a href="./android.php">- Android Developing</a></li>
+                                            <li><a href="./graphic.php">- Graphic Designing</a></li>
+                                            <li><a href="./ios.php">- iOS Developing</a></li>
+                                            <li><a href="./website.php">- Website Developing</a></li>
+                                            <li><a href="./video.php">- Video Editing</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="./contact.php">Contact</a></li>
+                                </ul>
+    
+                                <!-- Profile -->
+                                <div class="get-a-quote">
+                                    <a href="#" class="btn uza-btn">Box</a>
+                                </div>
+    
+                                <!-- Login / Register -->
+                                <div class="login-register-btn mx-3">
+                                    <a href="login.php">Logout<i class="icon_lock_alt"></i></a>    
+                                </div>
+    
+                                <!-- Search Icon -->
+                                <div class="search-icon" data-toggle="modal" data-target="#searchModal">
+                                    <i class="icon_search"></i>
+                                </div>
+                            </div>
+                            <!-- Nav End -->
+    
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+        <!-- ***** Header Area End ***** -->
+
+    <!-- ***** Breadcrumb Area Start ***** -->
+    <div class="breadcrumb-area">
+        <div class="container h-100">
+            <div class="row h-100 align-items-end">
+                <div class="col-12">
+                    <div class="breadcumb--con">
+                        <h2 class="title">Profile</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item"><a href="index.php"><i class="fa fa-home"></i> Home</a></li>
+                                <li class="breadcrumb-item active" aria-current="page">Profile</li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Background Curve -->
+        <div class="breadcrumb-bg-curve">
+            <img src="./img/core-img/curve-5.png" alt="">
+        </div>
+    </div>
+    <!-- ***** Breadcrumb Area End ***** -->
+
+    <!-- ***** Profile Area Start ***** -->
 	<div id="page">	
 	<header id="fh5co-header" class="fh5co-cover js-fullheight" role="banner" style="background-image:url(images/cover_bg_3.jpg);" data-stellar-background-ratio="0.5">
 		<div class="overlay"></div>
@@ -85,7 +231,6 @@ require_once ('class.Database.php');
 					<div class="display-t js-fullheight">
 						<div class="display-tc js-fullheight animate-box" data-animate-effect="fadeIn">
 							<div class="profile-thumb" style="background: url(images/user-3.jpg);"></div>
-							<p><a href="./index.php" class="btn btn-default btn-lg">Home</a></p>
 							<h1><span>Louie Jie Mahusay</span></h1>
 							<h3><span>Web Developer </span></h3>
 							<p>
@@ -233,9 +378,134 @@ require_once ('class.Database.php');
 	
 
 
-	<div class="gototop js-top">
-		<a href="#" class="js-gotop"><i class="icon-arrow-up22"></i></a>
-	</div>
+	<!-- ***** Profile Area End ***** -->
+
+    <!-- ***** Footer Area Start ***** -->
+    <footer class="footer-area section-padding-80-0">
+        <div class="container">
+            <div class="row justify-content-between">
+
+                <!-- Single Footer Widget -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-footer-widget mb-80">
+                        <!-- Widget Title -->
+                        <h4 class="widget-title">Contact Us</h4>
+
+                        <!-- Footer Content -->
+                        <div class="footer-content mb-15">
+                            <h3>(+65) 1234 5678</h3>
+                            <p>40 Baria Sreet 13/2 NY City, US <br> hello.colorlib@gmail.com</p>
+                        </div>
+                        <p class="mb-0">Mon - Fri: 9:00 - 19:00 <br>
+                            Closed on Weekends</p>
+                    </div>
+                </div>
+
+                <!-- Single Footer Widget -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-footer-widget mb-80">
+                        <!-- Widget Title -->
+                        <h4 class="widget-title">Quick Link</h4>
+
+                        <!-- Nav -->
+                        <nav>
+                            <ul class="our-link">
+                                <li><a href="#">About Us</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Forum Registeration</a></li>
+                                <li><a href="#">Forum Sign In</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+                <!-- Single Footer Widget -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-footer-widget mb-80">
+                        <!-- Widget Title -->
+                        <h4 class="widget-title">Resources</h4>
+
+                        <!-- Nav -->
+                        <nav>
+                            <ul class="our-link">
+                                <li><a href="#">Customer Support</a></li>
+                                <li><a href="#">Privacy</a></li>
+                                <li><a href="#">Media &amp; Press</a></li>
+                                <li><a href="#">Our Team</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+
+                <!-- Single Footer Widget -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-footer-widget mb-80">
+                        <!-- Widget Title -->
+                        <h4 class="widget-title">About Us</h4>
+                        <p>Integer vehicula mauris libero, at molestie eros imperdiet sit amet.</p>
+
+                        <!-- Copywrite Text -->
+                        <div class="copywrite-text mb-30">
+                            <p>&copy; Copyright 2018 <a href="#">Colorlib</a>.</p>
+                        </div>
+
+                        <!-- Social Info -->
+                        <div class="footer-social-info">
+                            <a href="#" class="facebook" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook"></i></a>
+                            <a href="#" class="twitter" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter"></i></a>
+                            <a href="#" class="pinterest" data-toggle="tooltip" data-placement="top" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+                            <a href="#" class="instagram" data-toggle="tooltip" data-placement="top" title="Instagram"><i class="fa fa-instagram"></i></a>
+                            <a href="#" class="youtube" data-toggle="tooltip" data-placement="top" title="YouTube"><i class="fa fa-youtube-play"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+ <div class="row" style="margin-bottom: 30px;">
+                
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </div>
+
+        </div>
+    </footer>
+    <!-- ***** Footer Area End ***** -->
+
+    <!-- ******* All JS Files ******* -->
+    <!-- jQuery js -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- All js -->
+    <script src="js/uza.bundle.js"></script>
+    <!-- Active js -->
+    <script src="js/default-assets/active.js"></script>
+<!--/////////////////////////////////////////////////-->
+    <!-- jQuery -->
+	<script src=".profile-master/js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src=".profile-master/js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src=".profile-master/js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src=".profile-master/js/jquery.waypoints.min.js"></script>
+	<!-- Stellar Parallax -->
+	<script src=".profile-master/js/jquery.stellar.min.js"></script>
+	<!-- Easy PieChart -->
+	<script src=".profile-master/js/jquery.easypiechart.min.js"></script>
+	<!-- Google Map -->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
+	<script src=".profile-master/js/google_map.js"></script>
+	
+	<!-- Main -->
+	<script src=".profile-master/js/main.js"></script>
+<!--//////////////////////////////////////////////-->
 	
 	<!-- jQuery -->
 	<script src="jsProfile/jquery.min.js"></script>
