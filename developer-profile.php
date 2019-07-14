747cdc3ab52396b7b55205e71807f43be58a9e33
NadeeshaGarusinghe <nadeeshagarusinghe.17@cse.mrt.ac.lk>
2019-07-14 12:47:42 +0530

    remove request button

--- developer-profile.php
+++ developer-profile.php
@@ -427,7 +427,7 @@ $sss.=" ";
 			<div class="row animate-box">
 				<div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
 					<h2>Hire Me!</h2>
-					<p><a href="#" class="btn btn-default btn-lg">Send Request</a></p>
+					
 				</div>
 			</div>
 		</div>
